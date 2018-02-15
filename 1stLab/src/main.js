5f5b7aef2af00afc4ac7ba24c2d3f8cca0cec771
Arut Thanomwatana <arut.t@ku.th>
2018-02-15 17:39:20 +0700

    Finish

---
+++ 1stLab/src/main.js
@@ -0,0 +1,24 @@
+function onGameHover() {
+  document.getElementById("gaming").innerHTML = "I have been interest in gaming since I was young. <br> Gaming make me feel relax and feel like I can take a break from problem at hand"
+}
+function onGameOut() {
+  document.getElementById("gaming").innerHTML = "<b>Gaming</b>"
+}
+function onMovieHover() {
+  document.getElementById("movie").innerHTML = "Movie and Animation Production get my interest in the difficulty to do it. <br> Sometime I use my free time to practice some video cut and edit."  
+}
+function onMovieOut() {
+  document.getElementById("movie").innerHTML = "<b>Movie and Animation Production</b>"
+}
+function onPsychologyHover() {
+  document.getElementById("psychology").innerHTML = "Psycholgy is the most field of study that I want to study into it. I am interest in how would one people think and react about the situation"  
+}
+function onPsychologyOut() {
+  document.getElementById("psychology").innerHTML = "<b>Psychology</b>"
+}
+function onMusicHover() {
+  document.getElementById("music").innerHTML = "I usually sing most of the time. Singing make my mind seem more focus (while distracting other haha XD) <br> but the most thing I love about music is orchestra."  
+}
+function onMusicOut() {
+  document.getElementById("music").innerHTML = "<b>Music</b>"
+}
